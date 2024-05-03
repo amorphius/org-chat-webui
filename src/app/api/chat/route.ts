ac30c98d05e64aa653aaf15086625bb350cfbfea
Vitalii Kors <aqqs@outlook.com>
2024-05-03 08:42:11 +0200

    throw error when OPENAI_API key is not set

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
     const { messages } = await req.json();
     
     if(!process.env.OPENAI_API_KEY) {
-        return new Error('Missing OPENAI_API_KEY')
+        throw new Error('OPENAI_API_KEY is not set');
     }
 
     const response = await openai.chat.completions.create({
